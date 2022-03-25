085e728d18465cc227362c53571312888e402400
yining wang <ywang@lcs.on.ca>
2022-03-25 11:40:22 -0400

    added the structure and the list of the menu

---
+++ Shared/Model/Menu.swift
@@ -0,0 +1,20 @@
+//
+//  Menu.swift
+//  ApCreateTask (iOS)
+//
+//  Created by Yining Wang on 2022-03-25.
+//
+
+import Foundation
+
+struct Menu : Codable, Identifiable{
+    var id:Int
+    var date:String
+    var breakfast:String
+    var lunch:String
+    var dinner:String
+    
+}
+ 
+
+//Create the structure of the menu
---
+++ Shared/Model/Menus.swift
@@ -0,0 +1,13 @@
+//
+//  Menus.swift
+//  ApCreateTask (iOS)
+//
+//  Created by Yining Wang on 2022-03-25.
+//
+
+import Foundation
+
+struct MealMenu: Codable {
+    var list:[Menu]
+}
+// Make the menu as a list
