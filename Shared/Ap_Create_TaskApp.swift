1cd8993b88dd4334aa06cbe58fb7bd89ea952e43
yining wang <ywang@lcs.on.ca>
2022-03-25 11:31:23 -0400

    Initial Commit

---
+++ Shared/Ap_Create_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ap_Create_TaskApp.swift
+//  Shared
+//
+//  Created by Yining Wang on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct Ap_Create_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
